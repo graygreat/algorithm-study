4042a1f776a7552226bfc9476ca20f31e16bc8bd
oopsys6 <operasystem6@naver.com>
2020-02-15 01:14:08 +0900

    BaekJoon Algorithm

---
+++ Pro_11654/Main.java
@@ -0,0 +1,13 @@
+package Pro_11654;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+
+        char result = scan.next().charAt(0);
+
+        System.out.println((int)result);
+    }
+}
