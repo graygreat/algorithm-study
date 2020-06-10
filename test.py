017f62b40a96cbd4dae119336862f0189840a104
oopsys6 <operasystem6@naver.com>
2020-06-10 22:02:08 +0900

    update

---
+++ test.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+length = n * 2 - 1
+
+for i in range(n):
+    for j in range(i + 1):
+        print("*", end = ' ')
+    print()
+
+for i in range(n - 1, 0, -1):
+    for j in range(i):
+        print("*", end=' ')
+    print()
+
