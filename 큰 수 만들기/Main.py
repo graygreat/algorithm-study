0da40ce26492dd42edef163cc86deb0f944287c0
oopsys6 <operasystem6@naver.com>
2020-06-10 21:39:47 +0900

    Programmers Problem 큰 수 만들기

--- 큰 수 만들기/Main.py
+++ 큰 수 만들기/Main.py
@@ -1,20 +1,17 @@
 def solution(number, k):
-    answer = ''
-    length = len(number)
-    maxIndex = 0
-    j = k
-    while(True):
-        if (len(answer) == (length - k)):
-            return str(''.join(answer))
-
-        numberSlicing = number[maxIndex: j + 1]
-        maxNum = max(numberSlicing)
-        maxIndex += numberSlicing.index(maxNum) + 1
-        answer += maxNum
-        j = j + 1
-
+    collected = []
+    for i, num in enumerate(number):
+        while len(collected) > 0 and collected[-1] < num and k > 0:
+            collected.pop()
+            k -= 1
+        collected.append(num)
+    if k > 0:
+        collected = collected[:-k]
+    answer = ''.join(collected)
+    return answer
 
 number = "4177252841"
 k = 4
 
 print(solution(number, k))
+
