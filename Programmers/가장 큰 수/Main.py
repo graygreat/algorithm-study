260c596997fcde6d4329f058aa7ae112e5d12cf5
oopsys6 <operasystem6@naver.com>
2020-06-17 23:58:44 +0900

    Programmers Problem 가장 큰 수

--- Programmers/가장 큰 수/Main.py
+++ Programmers/가장 큰 수/Main.py
@@ -1,3 +1,5 @@
+# 출처 : https://programmers.co.kr/learn/courses/30/lessons/42746
+
 def solution(numbers):
     numbers = list(map(str, numbers))
     numbers.sort(key = lambda x : x*3, reverse=True)
