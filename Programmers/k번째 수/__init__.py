008d12ad377c64d19bae827d768348c7ddc8ba37
oopsys6 <operasystem6@naver.com>
2020-07-02 00:54:52 +0900

    Programmers Problem K번째수

--- Programmers/k번째 수/__init__.py
+++ Programmers/k번째 수/__init__.py
@@ -1,14 +1,13 @@
 # 출처 : https://programmers.co.kr/learn/courses/30/lessons/42748
-# 첫번째 소스
+
 def solution(array, commands):
     answer = []
     sliceArray = []
     for i in range(len(commands)):
         sliceArray.append(array[commands[i][0] - 1 : commands[i][1]])
-
-    for i in range(len(sliceArray)):
-        resultArray = sorted(sliceArray[i])
+        resultArray = sorted(sliceArray[0])
         answer.append(resultArray[commands[i][2] - 1])
+        sliceArray.pop(0)
 
     return answer
 
