e633303882e270cde9d6fc5e723d9c58f7f19523
oopsys6 <operasystem6@naver.com>
2020-07-17 00:20:44 +0900

    BaekJoon Probelm 1448

--- BaekJoon/Pro_1448/Main.py
+++ BaekJoon/Pro_1448/Main.py
@@ -1,13 +1,33 @@
 # 출처 : https://www.acmicpc.net/problem/1448
 
+import sys
+#######
+#해결1#
+#######
+N = int(input())
+arr = []
+
+for _ in range(N):
+    arr.append(int(sys.stdin.readline()))
+arr.sort(reverse=True)
+
+for i in range(len(arr) - 2):
+    if arr[i + 1] + arr[i + 2] > arr[i]:
+        print(arr[i] + arr[i + 1] + arr[i + 2])
+        sys.exit()
+
+print(-1)
+
+#######
+#해결2#
+#######
 N = int(input())
 arr = []
 result = 0
 
 for _ in range(N):
-    arr.append(int(input()))
+    arr.append(int(sys.stdin.readline()))
 arr.sort(reverse=True)
-a = max(arr)
 
 while(True):
     if arr[0] < (arr[1] + arr[2]):
