09d423462d0586f2dd31dc92b0a085c417bf4bf3
oopsys6 <operasystem6@naver.com>
2020-07-20 00:50:16 +0900

    fail test case 5

--- Programmers/다리를 지나는 트럭/Main.py
+++ Programmers/다리를 지나는 트럭/Main.py
@@ -1,17 +1,19 @@
+# 출처 : https://programmers.co.kr/learn/courses/30/lessons/42583
 def solution(bridge_length, weight, truck_weights):
-    bridge = [0] * bridge_length
-    second = 0
-    while bridge:
-        second += 1
-        bridge.pop(0)
-        if truck_weights:
-            if sum(bridge) + truck_weights[0] <= weight:
-                bridge.append(truck_weights.pop(0))
-            else:
-                bridge.append(0)
+    time = 0
+    bridge_queue = [0] * bridge_length
 
-    return second
+    while(True):
+        time += 1
+        bridge_queue.pop(0)
 
-# print(solution(100, 100, [10]))
-print(solution(2, 10, [7, 4, 5, 6]))
+        if truck_weights and (sum(bridge_queue) + truck_weights[0] <= weight):
+            bridge_queue.append(truck_weights.pop(0))
+        else:
+            bridge_queue.append(0)
+        if sum(bridge_queue) == 0:
+            return time
 
+
+# print(solution(2, 10, [7, 4, 5, 6]))
+print(solution(100, 100, [10]))
