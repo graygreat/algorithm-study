ea035682c1d34c30a9fef7f67bc12dac75c51032
oopsys6 <operasystem6@naver.com>
2020-07-20 01:26:24 +0900

    success solution

--- Programmers/다리를 지나는 트럭/Main.py
+++ Programmers/다리를 지나는 트럭/Main.py
@@ -2,18 +2,23 @@
 def solution(bridge_length, weight, truck_weights):
     time = 0
     bridge_queue = [0] * bridge_length
-
-    while(True):
+    sum_bridge_queue = 0
+    while(bridge_queue):
         time += 1
-        bridge_queue.pop(0)
+        bqp = bridge_queue.pop(0)
+        sum_bridge_queue -= bqp
 
-        if truck_weights and (sum(bridge_queue) + truck_weights[0] <= weight):
-            bridge_queue.append(truck_weights.pop(0))
-        else:
-            bridge_queue.append(0)
-        if sum(bridge_queue) == 0:
-            return time
+        if truck_weights:
+            if sum_bridge_queue + truck_weights[0] <= weight:
+                twp = truck_weights.pop(0)
+                sum_bridge_queue += twp
+                bridge_queue.append(twp)
+            else:
+                bridge_queue.append(0)
+    return time
 
 
-# print(solution(2, 10, [7, 4, 5, 6]))
-print(solution(100, 100, [10]))
+
+print(solution(2, 10, [7, 4, 5, 6]))
+# print(solution(100, 100, [10]))
+# print(solution(1, 1, [1]))
