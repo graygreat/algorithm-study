08fe0f056e275f309616aab5271529b82e09546b
oopsys6 <operasystem6@naver.com>
2020-08-19 18:12:43 +0900

    fail code update

---
+++ Programmers/조이스틱/Main.py
@@ -0,0 +1,14 @@
+# 출처 : https://programmers.co.kr/learn/courses/30/lessons/42860
+
+def solution(name):
+    answer = 0
+    for i in range(len(name)):
+        if ord(name[i]) == 65:
+            answer -= 1
+        if ord(name[i]) > 78:
+            answer += (90 - ord(name[i]) + 1)
+        else:
+            answer += (ord(name[i]) - 65)
+        # print(i, answer)
+    return answer + len(name) - 1
+print(solution("JAN"))
