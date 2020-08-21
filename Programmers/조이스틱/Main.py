ad24dda5f923348f1b18f9a90f39a55b0940c684
oopsys6 <operasystem6@naver.com>
2020-08-21 13:30:02 +0900

    fail code update

--- Programmers/조이스틱/Main.py
+++ Programmers/조이스틱/Main.py
@@ -1,14 +1,18 @@
 # 출처 : https://programmers.co.kr/learn/courses/30/lessons/42860
 
+# print(ord('N'))
 def solution(name):
     answer = 0
     for i in range(len(name)):
-        if ord(name[i]) == 65:
-            answer -= 1
-        if ord(name[i]) > 78:
-            answer += (90 - ord(name[i]) + 1)
+        if name[i] == 'A':
+            continue
+        if ord(name[i]) < 78:
+            answer += ord(name[i]) - ord('A')
         else:
-            answer += (ord(name[i]) - 65)
-        # print(i, answer)
-    return answer + len(name) - 1
-print(solution("JANA"))
+            answer += ord('Z') - ord(name[i]) + 1
+        if i != (len(name) - 1):
+            answer += 1
+        print(answer)
+    return answer
+
+print(solution("AAB"))
