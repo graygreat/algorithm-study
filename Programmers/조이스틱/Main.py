9b3af6b5ea400b3bedc36a0ce63d796f05186627
oopsys6 <operasystem6@naver.com>
2020-08-24 09:47:39 +0900

    success code update

--- Programmers/조이스틱/Main.py
+++ Programmers/조이스틱/Main.py
@@ -1,18 +1,29 @@
 # 출처 : https://programmers.co.kr/learn/courses/30/lessons/42860
 
-# print(ord('N'))
 def solution(name):
     answer = 0
-    for i in range(len(name)):
-        if name[i] == 'A':
-            continue
-        if ord(name[i]) < 78:
-            answer += ord(name[i]) - ord('A')
+    name = list(name)
+    index = 0
+    while(True):
+        right = 1
+        left = 1
+        if name[index] != 'A':
+            updown = min(ord(name[index])-ord('A'), (ord('Z')-ord(name[index])+1))
+            answer += updown
+        name[index] = 'A'
+        if name == ["A"] * len(name): break
+        for i in range(1, len(name)):
+            if name[index+i] == "A": right += 1
+            else: break
+        for i in range(1, len(name)):
+            if name[index-i] == "A": left += 1
+            else: break
+        if right > left:
+            answer += left
+            index -= left
         else:
-            answer += ord('Z') - ord(name[i]) + 1
-        if i != (len(name) - 1):
-            answer += 1
-        print(answer)
+            answer += right
+            index += right
     return answer
 
 print(solution("AAB"))
