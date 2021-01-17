4737df10e975cf1053c3ab1d33d0c1b1d8538b65
oopsys6 <operasystem6@naver.com>
2021-01-17 21:38:47 +0900

    feat(LadderClimbing): 사다리타기 문제 풀이(실패)

---
+++ BaekJoon/Implementation/LadderClimbing.py
@@ -0,0 +1,41 @@
+#https://www.acmicpc.net/problem/2469
+import string, sys
+
+def swap(a, i):
+    temp = a[i]
+    a[i] = a[i + 1]
+    a[i + 1] = temp
+
+k = int(sys.stdin.readline())
+n = int(sys.stdin.readline())
+order_result = list(map(str, sys.stdin.readline().rstrip()))
+alphabet = list(string.ascii_uppercase[:k])
+
+ladder_array = []
+for i in range(n):
+    ladder_array.append(list(map(str, sys.stdin.readline().rstrip())))
+    if ladder_array[i][0] == '?':
+        break
+
+up_ladder_length = len(ladder_array)
+for i in range(up_ladder_length - 1):
+    for j in range(k - 1):
+        if ladder_array[i][j] == '-':
+            swap(alphabet, j)
+
+for i in range(n - up_ladder_length):
+    ladder_array.append(list(map(str, sys.stdin.readline().rstrip())))
+
+for i in range(len(ladder_array) - 1, up_ladder_length - 1, -1):
+    for j in range(k - 1):
+        if ladder_array[i][j] == '-':
+            swap(order_result, j)
+
+result = ""
+for i in range(k - 1):
+    if alphabet[i] == order_result[i]:
+        result += '*'
+    else:
+        result += '-'
+        swap(alphabet, i)
+print(result)
