b36608fa0a8ed1de0ea5dc3931b68435d92d1799
oopsys6 <operasystem6@naver.com>
2021-01-17 17:57:55 +0900

    style(자물쇠와 열쇠): 개행 수정

--- Programmers/[2020 KAKAO BLIND RECRUITMENT]/LockAndKey.py
+++ Programmers/[2020 KAKAO BLIND RECRUITMENT]/LockAndKey.py
@@ -8,6 +8,7 @@ def rotate_a_matrix_by_90_degree(a):
         for j in range(m):
             result[j][n - i - 1] = a[i][j]
     return result
+    
 def check(new_lock):
     lock_length = len(new_lock) // 3
     for i in range(lock_length, lock_length * 2):
