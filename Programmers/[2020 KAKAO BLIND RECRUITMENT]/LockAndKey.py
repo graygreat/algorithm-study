550247cfb3970f6aeb8aa2b20ca41a76280222cb
oopsys6 <operasystem6@naver.com>
2021-01-17 21:38:17 +0900

    docs(LockAndKey): 주석추가

--- Programmers/[2020 KAKAO BLIND RECRUITMENT]/LockAndKey.py
+++ Programmers/[2020 KAKAO BLIND RECRUITMENT]/LockAndKey.py
@@ -1,5 +1,6 @@
 # https://programmers.co.kr/learn/courses/30/lessons/60059
 
+# 시계 방향으로 90도 회전
 def rotate_a_matrix_by_90_degree(a):
     n = len(a)
     m = len(a[0])
@@ -8,7 +9,8 @@ def rotate_a_matrix_by_90_degree(a):
         for j in range(m):
             result[j][n - i - 1] = a[i][j]
     return result
-    
+
+# key가 모두 1인지 확인
 def check(new_lock):
     lock_length = len(new_lock) // 3
     for i in range(lock_length, lock_length * 2):
@@ -17,10 +19,11 @@ def check(new_lock):
                 return False
     return True
 
+
 def solution(key, lock):
     n = len(lock)
     m = len(key)
-    new_lock = [[0] * (n * 3) for _ in range(n * 3)]
+    new_lock = [[0] * (n * 3) for _ in range(n * 3)]    # 자물쇠의 범위를 3배 늘려준다. (이동을 위해)
     for i in range(n):
         for j in range(n):
             new_lock[i + n][j + n] = lock[i][j]
@@ -29,11 +32,13 @@ def solution(key, lock):
         key = rotate_a_matrix_by_90_degree(key)
         for x in range(n * 2):
             for y in range(n * 2):
+                # 키를 넣는다.
                 for i in range(m):
                     for j in range(m):
                         new_lock[x + i][y + j] += key[i][j]
                 if check(new_lock) == True:
                     return True
+                # 키를 다시 뺸다
                 for i in range(m):
                     for j in range(m):
                         new_lock[x + i][y + j] -= key[i][j]
