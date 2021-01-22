f85a800324fee18eff6e64b7d7ad4b5d9b22666b
oopsys6 <operasystem6@naver.com>
2021-01-23 00:43:13 +0900

    fix(크레인 인형뽑기 게임): 틀림
    재 수정 해야함. 자고 일어나서 하자...

--- Programmers/[2019 카카오 개발자 겨울 인텁십]/크레인 인형뽑기 게임/Main.py
+++ Programmers/[2019 카카오 개발자 겨울 인텁십]/크레인 인형뽑기 게임/Main.py
@@ -1,21 +1,30 @@
+from collections import deque
+
 def solution(board, moves):
-    tempArr = []
-    answer = 0
-    for i in range(len(moves)):
-        for j in range(len(board)):
-            if(board[j][moves[i] - 1] > 0):
-                tempArr.append(board[j][moves[i] - 1])
-                board[j][moves[i] - 1] = 0
-                if(len(tempArr) >= 2 and tempArr[-1] == tempArr[-2]):
-                    tempArr.pop()
-                    tempArr.pop()
-                    answer += 2
+    length = len(board)
+    storage = deque()
+    previous = -1
+    count = 0
+    for move in moves:
+        for i in range(length):
+            doll = board[i][move - 1]
+            if doll == 0:
+                continue
+            if previous == doll:
+                storage.pop()
+                count += 2
+                previous = storage[-1]
                 break
+            if doll:
+                storage.append(doll)
+                board[i][move - 1] = 0
+                previous = doll
+                break
+            previous = doll
+    return count
 
-    return answer
+board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
+moves = [1,5,3,5,1,2,1,4]
 
-# board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
-# moves = [1,5,3,5,1,2,1,4]
-#
-# print(solution(board, moves))
+print(solution(board, moves))
 
