8b147806d2949bef4094fbd2f3ed446598223608
oopsys6 <operasystem6@naver.com>
2021-01-27 20:33:06 +0900

    style(MenuRenewal): print 제거

--- Programmers/[2021 KAKAO BLIND RECRUITMENT]/MenuRenewal.py
+++ Programmers/[2021 KAKAO BLIND RECRUITMENT]/MenuRenewal.py
@@ -13,10 +13,8 @@ def solution(orders, course):
         if len(count_list) > 1 and max(count_list.values()) > 1:
             for count in count_list:
                 if count_list[count] == max(count_list.values()):
-                    print(count)
                     answer.append(''.join(count))
     return sorted(answer) 
-
 
 # print(solution(["ABCFG",  "CDE", "AC", "ACDE", "BCFG", "ACDEH"], [2,3,4]))
 print(solution(["XYZ", "XWY", "WXA"], [2, 3, 4]))
