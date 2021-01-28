a57b63784158489576b9bf29f65f26ab6f17669a
oopsys6 <operasystem6@naver.com>
2021-01-28 20:17:34 +0900

    refactor(LostParenthesis): 잘 짜진 소스 풀이
    괜찮아 보이는 풀이 보고 다시 풀어봄

--- BaekJoon/Greedy/ LostParenthesis.py
+++ BaekJoon/Greedy/ LostParenthesis.py
@@ -1,8 +1,9 @@
 # https://www.acmicpc.net/problem/1541
+
+# 내 소스
 import re
 
 data = input()
-# print(data.split(''))
 number_data = list(map(int, re.split("\W+", data)))
 sign_data = re.split("\d+", data)[1:-1]
 
@@ -22,3 +23,18 @@ for i in range(1, len(number_data)):
         continue
 
 print(result)
+
+''' 잘 짜진 소스
+data_set = input().split('-')
+sum_data = []
+number_data = []
+
+for data in data_set:
+    number_data = list(map(int, data.split('+')))
+    sum_data.append(sum(number_data))
+
+result = sum_data[0]
+for data in sum_data[1:]:
+    result -= data
+print(result)    
+'''
