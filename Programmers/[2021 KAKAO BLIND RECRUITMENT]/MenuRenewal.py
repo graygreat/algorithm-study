39e34d670254610fa8c786f0c68975855f966418
oopsys6 <operasystem6@naver.com>
2021-01-28 15:31:16 +0900

    docs(MenuRenewal): 주석 처리

--- Programmers/[2021 KAKAO BLIND RECRUITMENT]/MenuRenewal.py
+++ Programmers/[2021 KAKAO BLIND RECRUITMENT]/MenuRenewal.py
@@ -7,14 +7,14 @@ def solution(orders, course):
     for c in course:
         array = []
         for order in orders:
-            combination = list(combinations(sorted(order), c))
+            combination = list(combinations(sorted(order), c))      #order를 정렬하여 조합
             array += combination
-        count_list = Counter(array)
+        count_list = Counter(array)                                 #중복된 menu count
         if len(count_list) > 1 and max(count_list.values()) > 1:
             for count in count_list:
-                if count_list[count] == max(count_list.values()):
+                if count_list[count] == max(count_list.values()):   #menu 조합의 개수와 menu 조합 중 가장 많이 나온 값의 개수 비교
                     answer.append(''.join(count))
     return sorted(answer) 
 
-# print(solution(["ABCFG",  "CDE", "AC", "ACDE", "BCFG", "ACDEH"], [2,3,4]))
-print(solution(["XYZ", "XWY", "WXA"], [2, 3, 4]))
+print(solution(["ABCFG",  "CDE", "AC", "ACDE", "BCFG", "ACDEH"], [2,3,4]))
+# print(solution(["XYZ", "XWY", "WXA"], [2, 3, 4]))
