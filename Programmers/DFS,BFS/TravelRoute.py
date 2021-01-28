2399942d8633a949413d9b7ab97f56afeb6368a4
oopsys6 <operasystem6@naver.com>
2021-01-28 14:33:46 +0900

    refactor(TravelRoute): 정답
    어렵다 어려워ㅏ..

--- Programmers/DFS,BFS/TravelRoute.py
+++ Programmers/DFS,BFS/TravelRoute.py
@@ -1,16 +1,39 @@
 # https://programmers.co.kr/learn/courses/30/lessons/43164
 from collections import deque
 
+def solution(tickets):
+    answer = []
+
+    def dfs(start, used_ticket):
+        for i in range(len(tickets)):
+            if tickets[i][0] == start and i not in used_ticket:
+                used_ticket.append(i)
+                print(tickets[i][1])
+                dfs(tickets[i][1], used_ticket)
+                if len(tickets) == len(used_ticket):
+                    return
+                else:
+                    used_ticket.pop()
+
+    tickets = sorted(tickets, key = lambda x : (x[0], x[1]))
+    used_ticket = []
+    dfs("ICN", used_ticket)
+
+    answer = ["ICN"]
+    for ticket in used_ticket:
+        answer.append(tickets[ticket][1])
+
+    return answer
+'''
 def check(tickets, start):
     answer = []
     for i in range(len(tickets)):
         if tickets[i][0] == start:
             answer.append(tickets[i])
     result = sorted(answer, key = lambda x : x[1])
+    print(result)
     if len(result) > 1:
-        print(result)
         result.pop()
-    # print(tickets)
     tickets.pop(tickets.index(result[0]))
     return result[0][1], tickets
 
@@ -21,7 +44,7 @@ def solution(tickets):
         start, tickets = check(tickets, start)
         answer.append(start)
     return answer
-
+'''
 print(solution([['ICN','BOO' ], [ 'ICN', 'COO' ], [ 'COO', 'DOO' ], ['DOO', 'COO'], [ 'BOO', 'DOO'] ,['DOO', 'BOO'], ['BOO', 'ICN' ], ['COO', 'BOO']]))
 # print(solution([["ICN", "SFO"], ["SFO", "ICN"], ["ICN", "SFO"],["SFO","QRE"]]))
 # print(solution([["ICN", "A"], ["ICN", "A"], ["A", "ICN"]]))
