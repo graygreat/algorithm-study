37a0da27a5c92c0ed25721b0eaca91cac6429324
oopsys6 <operasystem6@naver.com>
2021-02-20 21:44:18 +0900

    dp 문제 풀이

--- BaekJoon/Dynamic-Programming/10844.py
+++ BaekJoon/Dynamic-Programming/10844.py
@@ -6,7 +6,7 @@ dp = []
 dp.append([1 for i in range(10)])
 for i in range(99):
     dp.append([0 for i in range(10)])
-
+ 
 for i in range(99):
     for j in range(10):
         if j >= 1:
--- BaekJoon/Dynamic-Programming/1463.py
+++ BaekJoon/Dynamic-Programming/1463.py
@@ -11,5 +11,4 @@ for i in range(2, n + 1):
     if i % 3 == 0:
         d[i] = min(d[i], d[i // 3] + 1)
 
-print(d)
 print(d[n])
