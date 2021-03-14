9278aec71cb0af38b8094a343f8da5734a59b8c5
oopsys6 <operasystem6@naver.com>
2021-03-15 00:56:45 +0900

    프로그래머스 레벨 1 풀기

---
+++ Programmers/practice/문자열을_정수로_바꾸기.py
@@ -0,0 +1,6 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12925
+
+def solution(s):
+    return int(s)
+
+print(solution("1234"))
---
+++ Programmers/practice/수박수박수박수박수박수?.py
@@ -0,0 +1,12 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12922
+
+def solution(n):
+    answer = ''
+    for i in range(n):
+        if i % 2 == 0:
+            answer += '수'
+        else:
+            answer += '박'
+    return answer
+
+print(solution(4))
