28199042e444b0a70960acc3db96a8244f4773bf
oopsys6 <operasystem6@naver.com>
2021-03-19 15:17:24 +0900

    feat: 시각 문제
    이코테 복습(구현)

---
+++ 이것이 코딩테스트다/복습/시각.py
@@ -0,0 +1,11 @@
+# 그리디 예제 4 - 2
+n = int(input())
+count = 0
+
+for h in range(0, n + 1):
+    for m in range(60):
+        for s in range(60):
+            if '3' in str(h) + str(m) + str(s):
+                count += 1
+
+print(count)
