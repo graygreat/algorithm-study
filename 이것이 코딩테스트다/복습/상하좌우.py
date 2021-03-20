a6bd8be554bab7197fff2b8a7a933905f241dbd4
oopsys6 <operasystem6@naver.com>
2021-03-20 16:05:12 +0900

    feat: 상하좌우 문제 풀이 복습

---
+++ 이것이 코딩테스트다/복습/상하좌우.py
@@ -0,0 +1,20 @@
+# 구현 예제  4-1
+
+n = int(input())
+direction = list(map(str, input().split()))
+x, y = 1, 1
+
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+move_types = ['L', 'R', 'U', 'D']
+
+for d in direction:
+    for i in range(len(move_types)):
+        if d == move_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+    x, y = nx, ny
+
+print(x, y)
