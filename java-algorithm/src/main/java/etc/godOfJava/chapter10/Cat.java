c6dcb59f20f40ed0ae23acd998e8d876a69474d6
oopsys6 <operasystem6@naver.com>
2021-04-20 18:24:19 +0900

    자바의 신 10장 실습

---
+++ java-algorithm/src/main/java/etc/godOfJava/chapter10/Cat.java
@@ -0,0 +1,15 @@
+package etc.godOfJava.chapter10;
+
+public class Cat extends Animal {
+
+
+    @Override
+    public void move() {
+        super.move();
+    }
+
+    @Override
+    public void eatFood() {
+        super.eatFood();
+    }
+}
---
+++ java-algorithm/src/main/java/etc/godOfJava/chapter10/Dog.java
@@ -0,0 +1,14 @@
+package etc.godOfJava.chapter10;
+
+public class Dog extends Animal {
+
+    @Override
+    public void move() {
+        super.move();
+    }
+
+    @Override
+    public void eatFood() {
+        super.eatFood();
+    }
+}
