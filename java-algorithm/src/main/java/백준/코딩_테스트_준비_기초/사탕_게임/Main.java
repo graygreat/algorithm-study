b2ea9fb093b2c861d2ab5a0162e3b2b4d86c6f33
oopsys6 <operasystem6@naver.com>
2021-06-28 17:19:13 +0900

    feat: 210628 알고리즘 문제 풀이

---
+++ java-algorithm/src/main/java/백준/코딩_테스트_준비_기초/사탕_게임/Main.java
@@ -0,0 +1,24 @@
+package 백준.코딩_테스트_준비_기초.사탕_게임;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        return 0;
+    }
+
+    // 오른쪽으로 위치 변경 하는 메소드
+
+    // 아래로 위치 변경 하는 메소드
+
+    // 가로, 세로 최대값 메소드
+}
