ba8c4d4409955734091c45ec9e029d40f0341f71
oopsys6 <operasystem6@naver.com>
2021-06-30 12:15:27 +0900

    feat: 210630 알고리즘 문제 풀이
    이코테 연습

--- java-algorithm/src/main/java/이것이코딩테스트다/greedy/큰_수의_법칙/Main.java
+++ java-algorithm/src/main/java/이것이코딩테스트다/greedy/큰_수의_법칙/Main.java
@@ -1,39 +1,47 @@
 package 이것이코딩테스트다.greedy.큰_수의_법칙;
 
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    private static Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
         Main main = new Main();
         System.out.println(main.solution());
     }
+
     public int solution() {
+        Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
         int k = scanner.nextInt();
 
-        int[] dataSet = new int[n];
-        for (int i = 0; i < n; i++) {
-            dataSet[i] = scanner.nextInt();
-        }
-        Arrays.sort(dataSet);
-        int firstValue = dataSet[n - 1];
-        int secondValue = dataSet[n - 2];
-        int answer = 0;
-        int count = k;
+        int[] data = new int[n];
 
+        for (int i = 0; i < n; i++) {
+            data[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(data);
+
+        int count = 0;
+        int answer = 0;
         for (int i = 0; i < m; i++) {
-            if (count == 0) {
-                answer += secondValue;
-                count = k;
+            if (count == k) {
+                answer += data[n - 2];
+                System.out.println(data[n - 2]);
+                count = 0;
             } else {
-                answer += firstValue;
-                count--;
+                answer += data[n - 1];
+                System.out.println(data[n - 1]);
             }
+            count += 1;
         }
         return answer;
     }
 }
+
+/**
+ 5 8 3
+ 2 4 5 4 6
+ */
--- java-algorithm/src/main/java/이것이코딩테스트다/implementation/왕실의_나이트/Main.java
+++ java-algorithm/src/main/java/이것이코딩테스트다/implementation/왕실의_나이트/Main.java
@@ -3,30 +3,31 @@ package 이것이코딩테스트다.implementation.왕실의_나이트;
 import java.util.Scanner;
 
 public class Main {
-    private static Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
         Main main = new Main();
         System.out.println(main.solution());
     }
 
     public int solution() {
-        String inputData = scanner.nextLine();
-        int row = inputData.charAt(1) - '0';
-        int column = inputData.charAt(0) - 'a' + 1;
+        Scanner scanner = new Scanner(System.in);
+
+        String data = scanner.next();
+        int column = data.charAt(0) - 'a';
+        int row = data.charAt(1) - '1';
 
         int[] dx = {-2, -1, 1, 2, 2, 1, -1, -2};
         int[] dy = {-1, -2, -2, -1, 1, 2, 2, 1};
 
-        int result = 0;
+        int answer = 0;
         for (int i = 0; i < 8; i++) {
-            int nextRow = row + dx[i];
-            int nextColumn = column + dy[i];
-
-            if (nextRow >= 1 && nextColumn >= 1 && nextColumn <= 8 && nextRow <= 8) {
-                result += 1;
+            int nx = row - dx[i];
+            int ny = column - dy[i];
+            if (nx < 0 || ny < 0 || nx >= 8 || ny >= 8) {
+                continue;
             }
+            answer += 1;
         }
-        return result;
+
+        return answer;
     }
 }
