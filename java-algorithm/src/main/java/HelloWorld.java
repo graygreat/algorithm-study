874c12a8b980fbe493bf95ef91a2aadb749532b9
oopsys6 <operasystem6@naver.com>
2021-09-17 21:04:38 +0900

    test code

--- java-algorithm/src/main/java/HelloWorld.java
+++ java-algorithm/src/main/java/HelloWorld.java
@@ -1,5 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world");
+
+    }
+
+    public int solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] inputData = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+        return 0;
+
     }
 }
